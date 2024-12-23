307a7ae8462711b8510982e7d0e895d5a46e4002
Tomer5635 <pigithepigman.mc@gmail.com>
2024-12-23 09:41:03 +0200

    did more trainer work, almost works. buffer won't load yet

--- Trainer.py
+++ Trainer.py
@@ -5,11 +5,13 @@ from DQN_Agent import DQN_Agent
 import graphics
 import os
 from ReplayBuffer import ReplayBuffer
+import wandb
 WIDTH , HEIGHT = 540,710
 def main ():
 
     pygame.init()
     path = "Data/parameters1"
+    bufferPath = "Data/buffer1"
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     pygame.display.set_caption('PAC MAN')
@@ -26,16 +28,28 @@ def main ():
     batch_size = 50
     buffer = ReplayBuffer()
     learning_rate = 0.00001
-    ephocs = 200000
+    epochs = 100
     start_epoch = 0
     C = 3
-    loss = torch.tensor(-1)
+    loss = torch.tensor(-1,dtype=torch.float32,requires_grad=True)
     avg = 0
     scores, losses, avg_score = [], [], []
     optim = torch.optim.Adam(player.DQN.parameters(), lr=learning_rate)
     # scheduler = torch.optim.lr_scheduler.StepLR(optim,100000, gamma=0.50)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optim,[5000*1000, 10000*1000, 15000*1000], gamma=0.5)
     step = 0
+    wandb.init(
+    # set the wandb project where this run will be logged
+    project="pacman-project",
+
+    # track hyperparameters and run metadata
+    config={
+    "learning_rate": learning_rate,
+    "architecture": "DQN",
+    "dataset": "PACMAN",
+    "epochs": epochs,
+    }
+)
 
     ######### checkpoint Load ############
     # checkpoint_path = "Data/checkpoint20.pth"
@@ -56,7 +70,7 @@ def main ():
 
     #################################
 
-    for epoch in range(start_epoch, ephocs):
+    for epoch in range(start_epoch, epochs):
         game = Game()
         state = game.state()
         gameTick=0
@@ -69,42 +83,45 @@ def main ():
             gameTick=gameTick%264
             ############## Sample Environement #########################
             if gameTick%6==0:
-                action = player.getAction(state=state, epoch=epoch)
-                graphics.Graphics.game_screen(screen,game)
+                action = player.getAction(state=state, epoch=epoch,train=False)
+                # graphics.Graphics.game_screen(screen,game)
                 gameTick,nextState,reward=game.tick(gameTick,action)
                 buffer.push(state, torch.tensor(action, dtype=torch.int64), torch.tensor(reward, dtype=torch.float32), 
                             nextState, torch.tensor(game.game_over!=False, dtype=torch.float32))
             else:
                 gameTick,midState,_=game.tick(gameTick,action)
-                graphics.Graphics.game_screen(screen,game)
+                # graphics.Graphics.game_screen(screen,game)
 
             if game.game_over:
                 best_score = max(best_score, game.points)
                 buffer.push(state, torch.tensor(action, dtype=torch.int64), torch.tensor(reward, dtype=torch.float32), midState, torch.tensor(game.game_over!=False, dtype=torch.float32))
-                graphics.Graphics.game_screen(screen,game)
-                pygame.display.update()
+                # graphics.Graphics.game_screen(screen,game)
+                # pygame.display.update()
                 break
             state = nextState
 
-            pygame.display.update()
-            clock.tick(60)
+            # pygame.display.update()
+            clock.tick(1000)
             
             if len(buffer) < 1000:
                 continue
     
             ############## Train ################
             states, actions, rewards, next_states, dones = buffer.sample(batch_size)
-            Q_values = player.Q(states, actions)
-            Q_hat_Values = player_hat.getActionValues(next_states)
+            Q_values = player.getActionValues(states)
+            Q_hat_Values = player_hat.getActionValues(states)
 
-            loss = player.DQN.loss(Q_values, rewards, Q_hat_Values, dones)
             loss.backward()
             optim.step()
             optim.zero_grad()
             scheduler.step()
-
+            wandb.log({ "loss": loss})
         if epoch % C == 0:
             player_hat.DQN.load_state_dict(player.DQN.state_dict())
+        if epoch%10 == 0:
+            player.save_param(path)
+            torch.save(buffer,bufferPath)
+            print ("epoch:",epoch,"  best score:",best_score)
 
         #########################################
         # print (f'epoch: {epoch} loss: {loss:.7f} LR: {scheduler.get_last_lr()} step: {step} ' \
